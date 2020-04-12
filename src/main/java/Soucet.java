fe2d46fc7c6881dcf99ed9de54b766baa87f1ce7
vj <vaclav.jurena@gmail.com>
2020-04-12 12:03:40 +0200

    add methods class

--- src/main/java/Soucet.java
+++ src/main/java/Soucet.java
@@ -1,6 +1,7 @@
 import java.lang.Math;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Soucet {
     public static void main(String[] args) {
@@ -35,14 +36,24 @@ public class Soucet {
         //soucet cisle vetsich nez tri
         List<Integer> pole = generateList(9, 8);
         System.out.println(pole);
+        System.out.println("Zadejte cislo: ");
+        Scanner sc = new Scanner(System.in);
+        int pocetcisel = sc.nextInt();
+        int soucetcisel = 0;
+        List<Integer> field = generateList(pocetcisel, 10);
+        for (int i=0; i<field.size(); i++);
+
+
+
+
+
 
 
     }
 
     static void rozdel() {
         List<Integer> pole = generateList(6, 10);
-        int x = pole.get(0);
-        int y = pole.get(0);
+
         List<Integer> mensinezpet = new ArrayList<Integer>();
         List<Integer> vetsinezpet = new ArrayList<Integer>();
 
@@ -54,21 +65,19 @@ public class Soucet {
             //pokud jsou hodnoty mensi nez 5
             if (pole.get(i) < 5) {
                 mensinezpet.add(pole.get(i));
-                System.out.println("Prvni iterace:" + mensinezpet);
-                System.out.println("Velikost pole s nazvem mensi je: " + mensinezpet.size() + " \n cisla jsou tyto: " + mensinezpet);
+               System.out.println("Prvni iterace:" + mensinezpet);
+               System.out.println("Velikost pole s nazvem mensi je: " + mensinezpet.size() + " \n cisla jsou tyto: " + mensinezpet);
 
 
             } else {
                 // pokud jsou hodnoty vetsi nez 5
                 vetsinezpet.add(pole.get(i));
-                System.out.println("druha iterace:" + vetsinezpet);
-                System.out.println("Velikost pole s nazvem vetsi je: " + vetsinezpet.size() + " \n cisla jsou tyto: " + vetsinezpet);
-
-
+               System.out.println("druha iterace:" + vetsinezpet);
+               System.out.println("Velikost pole s nazvem vetsi je: " + vetsinezpet.size() + " \n cisla jsou tyto: " + vetsinezpet);
             }
-
-
         }
+        System.out.println(mensinezpet);
+        System.out.println(vetsinezpet);
 
     }
     static void prumer() {
@@ -82,4 +91,15 @@ public class Soucet {
         }
         System.out.println((double) x / j);
     }
-}
+  /*  static void vypismatici(){
+        static int [][]generateMatrix(int m. int n);
+        int [][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+*/
+
+            }
+
+
+
+
