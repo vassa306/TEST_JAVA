0f4a012d8a632505b37b239d2136e2c64d6e6c75
vj <vassa306@gmail.com>
2022-01-29 16:53:24 +0100

    autolog to fb

---
+++ seleniumWebTutorial/src/main/java/BasicWeb/FirstTestClass.java
@@ -0,0 +1,53 @@
+package BasicWeb;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import java.util.concurrent.TimeUnit;
+
+
+public class FirstTestClass {
+
+    public static void main (String[] args){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-notifications");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\vassa\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        String baseUrl = "https://www.facebook.com";
+        driver.get(baseUrl);
+        driver.findElement(By.xpath("//button[@data-cookiebanner='accept_button']")).click();
+        WebElement emailfield = driver.findElement(By.id("email"));
+        emailfield.click();
+        emailfield.sendKeys("vassa306@gmail.com");
+
+        WebElement passfield = driver.findElement(By.id("pass"));
+        passfield.click();
+
+        passfield.sendKeys("Banicek1922!");
+         WebElement Login = driver.findElement(By.xpath("//button[@name='login']"));
+         Login.click();
+
+         driver.manage().timeouts().implicitlyWait(8,TimeUnit.SECONDS);
+
+        String title = driver.getTitle();
+        String URL = driver.getCurrentUrl();
+        System.out.println("Page title: " + title + " Page URL is: " +  URL);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
