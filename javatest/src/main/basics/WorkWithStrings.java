ef7294ccc99df5b9a7f35059a1cd3f0be2bf0e7c
vj <vassa306@gmail.com>
2022-02-13 12:08:04 +0100

    add string builder into WorkingWithClass.java

--- javatest/src/main/basics/WorkWithStrings.java
+++ javatest/src/main/basics/WorkWithStrings.java
@@ -6,21 +6,29 @@ import java.util.Locale;
 public class WorkWithStrings {
     public static void main(String[] args) {
         String s = " Hello worlds ";
-        String a = "Hello Earth";
+        String a = "Hello Earth ";
 
         CheckStringContent(s);
         toUppercase(s);
         toLowerCase(s);
+        // if String is lesser than int position java threw out of bounds exception
+        checkCharPosition(a,10);
+    }
+
+   public static void checkCharPosition(String a, int position) {
+        String upper = a.trim().toUpperCase();
+        String pos = String.valueOf(a.charAt(position));
+        StringBuilder builder = new StringBuilder();
+        builder.append("this is result" + "\n").append(pos);
+        System.out.println(builder.toString());
     }
 
     public static void toUppercase(String a) {
-
         String upper = a.trim().toUpperCase();
         System.out.print("string to uppercase is " + upper);
     }
 
     public static void toLowerCase(String a) {
-
         String lower = a.trim().toLowerCase();
         System.out.print("string to lowercase is " + lower);
     }
