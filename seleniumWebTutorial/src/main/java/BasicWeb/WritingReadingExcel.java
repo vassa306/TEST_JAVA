2be8d5ed9ff4836c981bc03f2fa0f934f1ec05cf
vj <vassa306@gmail.com>
2022-03-13 21:47:18 +0100

    update create XLS file

--- seleniumWebTutorial/src/main/java/BasicWeb/WritingReadingExcel.java
+++ seleniumWebTutorial/src/main/java/BasicWeb/WritingReadingExcel.java
@@ -1,4 +1,17 @@
 package BasicWeb;
 
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class WritingReadingExcel {
+     XSSFWorkbook workbook = new XSSFWorkbook();
+     XSSFSheet sheet0 = workbook.createSheet("FirstSheet");
+     Row row0 = sheet0.createRow(0);
+     Cell cell = row0.createCell(0);
+
+
+
 }
