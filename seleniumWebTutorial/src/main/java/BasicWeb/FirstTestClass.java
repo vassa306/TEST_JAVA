c80a923fde29ea930dc468efa07be092759eaf92
vassa <vassa306@gmail.com>
2022-04-04 16:17:36 +0200

    binary files

--- seleniumWebTutorial/src/main/java/BasicWeb/FirstTestClass.java
+++ seleniumWebTutorial/src/main/java/BasicWeb/FirstTestClass.java
@@ -1,6 +1,7 @@
 package BasicWeb;
 
 import Actions.TestActions;
+
 import org.openqa.selenium.WebDriver;
 
 
@@ -9,14 +10,16 @@ import java.io.IOException;
 import java.util.Properties;
 
 
-public class FirstTestClass extends TestActions {
+public class FirstTestClass extends TestActions{
+
+
 
 
 
     public static void main(String[] args) throws InterruptedException, IOException {
 
         Properties prop = new Properties();
-        try (FileInputStream file = new FileInputStream("C:\\Developer\\seleniumWebTutorial\\src\\main\\java\\config\\object.properties")) {
+        try (FileInputStream file = new FileInputStream("C:\\devops\\Test_JAVA\\seleniumWebTutorial\\src\\main\\java\\config\\object.properties")) {
             prop.load(file);
             System.out.println(prop.getProperty("login"));
             System.out.println(prop.getProperty("password"));
